851900daa6494d7c3708f97b50ac6b6d3fb48d1f
amclau <98709499+amclau@users.noreply.github.com>
2023-03-02 04:33:16 +0000

    "os prac"

---
+++ threadmain.java
@@ -0,0 +1,17 @@
+/**
+ * threadmain
+ */
+public class Booking extends Thread
+{   
+    int available=2,require;
+    booking(int n)
+    {
+
+    }
+
+} 
+public class threadmain {
+public static void main(String[] args) {
+    
+}
+}
